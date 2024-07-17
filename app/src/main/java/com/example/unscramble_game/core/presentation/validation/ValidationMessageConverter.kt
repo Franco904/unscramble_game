99c30b9a9ea1769a84bde5418267e2bfdfd3bac7
Franco904 <francostavares2003@gmail.com>
2024-07-17 11:22:11 -0300

    refactor: replace when clause by map

--- app/src/main/java/com/example/unscramble_game/core/presentation/validation/ValidationMessageConverter.kt
+++ app/src/main/java/com/example/unscramble_game/core/presentation/validation/ValidationMessageConverter.kt
@@ -5,9 +5,10 @@ import com.example.unscramble_game.R
 import com.example.unscramble_game.core.domain.validation.GUESS_IS_REQUIRED
 
 object ValidationMessageConverter {
+    private val presentationMessages = mapOf(
+        GUESS_IS_REQUIRED to R.string.unscramble_input_your_guess_before_submit
+    )
+
     @StringRes
-    fun String?.toPresentationMessage(): Int? = when (this) {
-        GUESS_IS_REQUIRED -> R.string.unscramble_input_your_guess_before_submit
-        else -> null
-    }
+    fun String?.toPresentationMessage(): Int? = presentationMessages[this]
 }
