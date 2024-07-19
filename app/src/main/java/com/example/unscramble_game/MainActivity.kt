8d6727b89acc69698a7a03722383ef4a21b31db0
Franco904 <francostavares2003@gmail.com>
2024-07-19 08:35:56 -0300

    refactor: rename feature module

--- app/src/main/java/com/example/unscramble_game/MainActivity.kt
+++ app/src/main/java/com/example/unscramble_game/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.unscramble_game.core.presentation.theme.UnscrambleGameTheme
-import com.example.unscramble_game.gameHistory.presentation.GameHistoryScreen
+import com.example.unscramble_game.previousGames.presentation.GameHistoryScreen
 import com.example.unscramble_game.gamePanel.presentation.GamePanelScreen
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/example/unscramble_game/previousGames/presentation/PreviousGamesScreen.kt
+++ app/src/main/java/com/example/unscramble_game/previousGames/presentation/PreviousGamesScreen.kt
@@ -1,9 +1,14 @@
-package com.example.unscramble_game.gameHistory.presentation
+package com.example.unscramble_game.previousGames.presentation
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -14,26 +19,53 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.unscramble_game.core.domain.models.Game
+import kotlinx.collections.immutable.persistentListOf
 
 @Composable
-fun GameHistoryScreen(
+fun PreviousGamesScreen(
     modifier: Modifier = Modifier,
     onBackNavigation: () -> Unit = {},
 ) {
     Scaffold(
         topBar = {
-            GameHistoryTopBar(onBackNavigation)
+            PreviousGamesTopBar(onBackNavigation)
         },
         containerColor = MaterialTheme.colorScheme.background,
         modifier = modifier
     ) { innerPadding ->
-        Spacer(modifier = Modifier.padding(innerPadding))
+        val games = persistentListOf<Game>(
+
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(
+                    start = 24.dp,
+                    top = innerPadding.calculateTopPadding(),
+                    end = 24.dp,
+                    bottom = 32.dp,
+                )
+                .background(MaterialTheme.colorScheme.primaryContainer)
+        ) {
+            Spacer(modifier = Modifier.height(24.dp))
+            Text(text = "17/7/2024")
+            Spacer(modifier = Modifier.height(24.dp))
+            Card(
+                modifier = Modifier.fillMaxSize()
+            ) {
+
+            }
+        }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GameHistoryTopBar(
+fun PreviousGamesTopBar(
     onBackNavigation: () -> Unit = {},
 ) {
     TopAppBar(
@@ -51,4 +83,10 @@ fun GameHistoryTopBar(
             containerColor = MaterialTheme.colorScheme.background,
         ),
     )
+}
+
+@Preview
+@Composable
+fun PreviousGamesScreenPreview() {
+    PreviousGamesScreen()
 }
