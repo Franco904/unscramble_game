2c645aa7f61fc631691f7f55819ba38f6c19dc41
Franco904 <francostavares2003@gmail.com>
2024-07-25 08:31:41 -0300

    refactor: update sharesheet builder

--- app/src/main/java/com/example/unscramble_game/core/presentation/utils/IntentUtils.kt
+++ app/src/main/java/com/example/unscramble_game/core/presentation/utils/IntentUtils.kt
@@ -2,28 +2,24 @@ package com.example.unscramble_game.core.presentation.utils
 
 import android.content.Context
 import android.content.Intent
+import android.content.IntentSender
 
 private const val ACTION_SEND_TYPE_TEXT_PLAIN = "text/plain"
 
 private fun Context.showShareSheet(
-    sendIntent: Intent,
-    title: String?,
+    intent: Intent,
+    title: String? = null,
+    sender: IntentSender? = null,
 ) {
-    val shareSheet = Intent.createChooser(sendIntent, title)
+    val shareSheet = Intent.createChooser(intent, title, sender)
     startActivity(shareSheet)
 }
 
-fun Context.showTextShareSheet(
-    title: String? = null,
-    subject: String? = null,
-    text: String? = null,
-) {
+fun Context.showTextShareSheet(text: String) {
     val sendIntent = Intent(Intent.ACTION_SEND).apply {
         type = ACTION_SEND_TYPE_TEXT_PLAIN
-
-        if (subject != null) putExtra(Intent.EXTRA_SUBJECT, subject)
-        if (text != null) putExtra(Intent.EXTRA_TEXT, text)
+        putExtra(Intent.EXTRA_TEXT, text)
     }
 
-    showShareSheet(sendIntent, title)
+    showShareSheet(sendIntent)
 }
