f723abdc4eb76258f0f6f476b32aa63065f1903f
Franco904 <francostavares2003@gmail.com>
2024-08-12 09:38:56 -0300

    refactor: improve separation of concerns

--- app/src/main/java/com/example/unscramble_game/MainActivity.kt
+++ app/src/main/java/com/example/unscramble_game/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.unscramble_game
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -21,21 +23,24 @@ class MainActivity : ComponentActivity() {
                 NavHost(
                     navController = navController,
                     startDestination = Routes.GamePanel,
-                ) {
-                    composable<Routes.GamePanel> {
-                        GamePanelScreen(
-                            onNavigateToGameHistory = {
-                                navController.navigate(Routes.PreviousGames)
-                            },
-                        )
-                    }
-                    composable<Routes.PreviousGames> {
-                        PreviousGamesScreen(
-                            onBackNavigation = navController::popBackStack,
-                        )
-                    }
-                }
+                    builder = { routes(navController) },
+                )
             }
+        }
+    }
+
+    private fun NavGraphBuilder.routes(navController: NavController) {
+        composable<Routes.GamePanel> {
+            GamePanelScreen(
+                onNavigateToGameHistory = {
+                    navController.navigate(Routes.PreviousGames)
+                },
+            )
+        }
+        composable<Routes.PreviousGames> {
+            PreviousGamesScreen(
+                onBackNavigation = navController::popBackStack,
+            )
         }
     }
 }
